31ba41e64d900dd52170df0b49e1376525cd0998
Lucas Lima <lucas.pinheiro-lima@hotmail.com>
2026-04-06 22:42:31 -0300

    message update

--- controllers/auth/registerController.js
+++ controllers/auth/registerController.js
@@ -9,7 +9,7 @@ exports.register = async (req, res) => {
   // verifica se já existe
   db.query("SELECT * FROM usuarios WHERE email = ?", [email], async (err, results) => {
     if (err) return res.status(500).json({ erro: "Erro de acesso ao banco de dados" });
-    if (results.length > 0) return res.status(400).json({ erro: "Em-ail já cadastrado" });
+    if (results.length > 0) return res.status(400).json({ erro: "E-mail já cadastrado" });
 
     const senhaHash = await bcrypt.hash(senha, 10);
 
