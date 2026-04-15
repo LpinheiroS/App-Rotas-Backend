9bc35effdbfd34f6c19ae21a7d42f4a78909beb0
Lucas Lima <lucas.pinheiro-lima@hotmail.com>
2026-04-14 21:07:14 -0300

    Configuração da API optimize AWS.

--- integrations/aws/optimize.js
+++ integrations/aws/optimize.js
@@ -1,13 +1,28 @@
-const { LocationClient } = require("@aws-sdk/client-location");
+const { GeoRoutesClient, OptimizeWaypointsCommand } = require("@aws-sdk/client-geo-routes");
 
-const awsLocationClient = new LocationClient({
-    region: process.env.AWS_REGION
+const client = new GeoRoutesClient({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
 });
 
 const optimizeWaypoints = async (coordinatesList) => {
-    // TODO: Configuração básica do integrador aws optimize-waypoints
-    // Aqui vai a chamada de API usando awsLocationClient
-    return { route: "rota otimizada (mock)" };
+    const command = new OptimizeWaypointsCommand(
+        {
+            "Origin": [-46.4938375, -23.4471931],
+            "Waypoints": [
+                { "Position": [-46.6032701, -23.4800747] },
+                { "Position": [-46.5766424, -23.5402524] },
+                { "Position": [-46.6163355, -23.5451136] },
+                { "Position": [-46.4938082, -23.4333006] }
+            ]
+        }
+    );
+    const response = await client.send(command);
+
+    return response;
 };
 
 module.exports = { optimizeWaypoints };
--- services/geocodeService.js
+++ services/geocodeService.js
@@ -2,13 +2,17 @@ const geocodeIntegration = require("../integrations/aws/geocode");
 
 const geocodeAddresses = async (addresses) => {
     const coordinates = [];
-    
+
     // Executa a API de geocode uma vez para cada endereço
     for (const address of addresses) {
-        const coord = await geocodeIntegration.geocodeAddress(address);
-        coordinates.push(coord);
+        const addressString = `${address.rua}, ${address.numero}, ${address.cep}`;
+        const coord = await geocodeIntegration.geocodeAddress(addressString);
+        const [lon, lat] = coord.ResultItems[0].Position;
+        const position = { lon, lat };
+
+        coordinates.push(position);
     }
-    
+
     return coordinates;
 };
 
